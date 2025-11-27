7b863d1aa631b2344c456ba23c17edc64da8f13c
Nimos087 <simon.pellegrims@gmail.com>
2025-11-27 12:03:42 +0100

    first local changes commited

---
+++ script.js
@@ -0,0 +1,72 @@
+const inputBox1 = document.getElementById("input-box");
+const listConstainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox1.value === ''){
+        alert("Good job I guess? You already completed nothing...");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox1.value;
+        listConstainer.appendChild(li);
+
+        let plus = document.createElement("img");
+        plus.src = "./images/plus.png"; 
+        plus.classList.add("plus");
+        let wrap = document.createElement("span");
+        wrap.classList.add("img-wrap"); //make and assign to "img-wrap" class
+        wrap.appendChild(plus)
+        li.appendChild(wrap);
+
+        let span = document.createElement("span");
+        span.classList.add("cross")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox1.value = '';
+}
+
+listConstainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){ //can stay for sublist
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.classList.contains("cross")){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    else if (e.target.classList.contains("plus")){
+        //make input appear
+        //(while not sent, change plus image to something else so you can click it to erase the input box)
+        //if input not empty and sent, 
+        
+        //if main list has no ul, make one and appendChild it to main li
+        //add li to it
+
+        //let subli = document.createElement("LI");
+        //subli.classList.add("sublist");
+
+        //saveData();
+
+    }
+}, false);
+
+inputBox1.addEventListener("keydown", function(e){
+    if (e.key === "Enter"){
+        addTask();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listConstainer.innerHTML);
+}
+function showTask(){
+    listConstainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+function delAll(){
+    localStorage.setItem("data", "");
+}
+//delAll();
---
+++ start_server.bat
@@ -0,0 +1,15 @@
+@echo off
+REM stop print
+
+REM cd jusqu'au dir actuel
+cd "%~dp0"
+
+REM Start the server in a new hidden window
+start "" "C:\Users\simon\AppData\Local\Programs\Python\Python313\pythonw.exe" -m http.server 8010
+
+
+REM Wait a short moment to ensure the server is running
+timeout /t 1 >nul
+
+REM Open the browser
+start "" http://localhost:8010/index.html
